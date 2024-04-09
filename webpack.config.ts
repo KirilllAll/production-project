4f1da83d923ff0e34f76200c4c91138215a6123b
Kirill_Aksenov <aksenovki@gmail.com>
2024-04-09 22:33:12 +1000

    changed gitignore

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,19 +1,19 @@
 import path from 'path';
 import webpack from 'webpack';
-import buildWebpackConfig from './config/build/webpackConfig';
+import { buildWebpackConfig } from './config/build/buildWebpackConfig';
 import { BuildEnv, BuildPaths } from './config/build/types/config';
 
 export default (env: BuildEnv): webpack.Configuration => {
 	const paths: BuildPaths = {
 		entry: path.resolve(__dirname, 'src', 'index.tsx'),
-		build: path.resolve(__dirname, 'build'),
+		build: path.resolve(__dirname, 'dist'),
 		html: path.resolve(__dirname, 'public', 'index.html'),
 	};
 
 	const mode = env.mode || 'development';
 	const PORT = env.port || 3000;
 
-    const isDev = mode === 'development';
+	const isDev = mode === 'development';
 
 	return buildWebpackConfig({
 		mode,
